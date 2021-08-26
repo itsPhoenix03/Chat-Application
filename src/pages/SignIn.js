8b0c5cedc4114b4f39b6ad15498f8235f1dd0ff3
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-08-26 15:23:27 +0530

    Designed the sign-in page and used firebase in it

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const signInProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('You are now Signed In!', 5000);
+    } catch (error) {
+      Alert.error(error.massage, 5000);
+    }
+  };
+
+  const onFacebookSignIn = () => {
+    signInProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  const onGoogleSignIn = () => {
+    signInProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col sx={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat!</h2>
+                <p>Prograssive Chat Application for People</p>
+              </div>
+
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" />
+                  Login with Facebook
+                </Button>
+
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" />
+                  Login with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
