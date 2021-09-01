d2defddc5d29a64ff3029f524fb0e19ef4ecf1a3
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-09-01 18:01:00 +0530

    created chat bottom; send messages and stored messages in database

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,7 +1,90 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
+import firebase from 'firebase/app';
+import { useParams } from 'react-router';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import { useProfile } from '../../../context/profile.context';
+import { database } from '../../../misc/firebase';
+
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
+}
 
 const Bottom = () => {
-  return <div>Bottom</div>;
+  const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { chatId } = useParams();
+  const { profile } = useProfile();
+
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onSendClick = async () => {
+    if (input.trim() === '') return;
+
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+
+    const update = {};
+
+    const messageId = database.ref(`messages`).push().key;
+
+    update[`/messages/${messageId}`] = msgData;
+    update[`/rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msgId: messageId,
+    };
+
+    setIsLoading(true);
+    try {
+      await database.ref().update(update);
+
+      setInput('');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      Alert.error(err.message);
+    }
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      ev.preventDefault();
+      onSendClick();
+    }
+  };
+
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder="Write your message..."
+          value={input}
+          onChange={onInputChange}
+          onKeyDown={onKeyDown}
+        />
+
+        <InputGroup.Button
+          color="cyan"
+          appearance="primary"
+          onClick={onSendClick}
+          disabled={isLoading}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default memo(Bottom);
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,10 +1,41 @@
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfoBtnModel from './RoomInfoBtnModel';
 
 const Top = () => {
+  const isMobile = useMediaQuery(`(min-width: 992px)`);
   const name = useCurrentRoom(v => v.name);
 
-  return <div>{name}</div>;
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4 className="text-disappear d-flex align-items-center">
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              !isMobile
+                ? 'd-inline-block p-0 mr-2 link-unstyle text-blue'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+
+        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModel />
+      </div>
+    </div>
+  );
 };
 
 export default memo(Top);
