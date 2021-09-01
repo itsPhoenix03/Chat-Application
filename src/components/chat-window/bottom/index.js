ac55ad80b169ba4b2dc85eda2ba9045fdce1210c
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-09-01 13:39:52 +0530

    created the chat-window; used the use-context-selector for creating the context

---
+++ src/components/chat-window/bottom/index.js
@@ -0,0 +1,7 @@
+import React, { memo } from 'react';
+
+const Bottom = () => {
+  return <div>Bottom</div>;
+};
+
+export default memo(Bottom);
---
+++ src/components/chat-window/messages/index.js
@@ -0,0 +1,7 @@
+import React, { memo } from 'react';
+
+const Messages = () => {
+  return <div>Messages</div>;
+};
+
+export default memo(Messages);
