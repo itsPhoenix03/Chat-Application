6c5b14ada1538f1fc698479d6d0e41dee3de722b
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-09-07 19:12:37 +0530

    removed a todo button

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -35,7 +35,6 @@ const Top = () => {
       </div>
 
       <div className="d-flex justify-content-between align-items-center">
-        <span>todo</span>
         <RoomInfoBtnModal />
       </div>
     </div>
